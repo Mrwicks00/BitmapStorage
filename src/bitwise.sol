0d1336888b0694bd760014dec20de79e5867792a
Mrwicks00 <victoradeoba83@gmail.com>
2025-03-29 00:03:52 +0100

    understanding bitmap storage

---
+++ src/bitwise.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Uint256Bitmap {
+    uint256 private bitmap;
+
+    function storeByte(uint8 value, uint8 index) external {
+        require(index < 32, "Index out of range");
+
+        uint256 shiftAmount = index * 8;
+        uint256 mask = ~(uint256(0xFF) << shiftAmount);
+
+        bitmap = (bitmap & mask) | (uint256(value) << shiftAmount);
+    }
+
+    function getByte(uint8 index) external view returns (uint8) {
+        require(index < 32, "Index out of range");
+
+        uint256 shiftAmount = index * 8;
+        return uint8((bitmap >> shiftAmount) & 0xFF);
+    }
+
+    function getAllBytes() external view returns (uint8[32] memory) {
+        uint8[32] memory values;
+        for (uint8 i = 0; i < 32; i++) {
+            values[i] = uint8((bitmap >> (i * 8)) & 0xFF);
+        }
+        return values;
+    }
+}
